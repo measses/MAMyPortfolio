f6e34159024b60b6ed035c26425438e03367bfcb
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-01-31 20:25:21 +0300

    Add to Interface classes

---
+++ DataLayer/Abstract/IGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Abstract;
+public interface IGeneric<T> where T : class
+{
+    void Add(T t);
+    void Delete(T t);
+    void Update(T t);
+    List<T> GetAll();
+    T GetById(int id);
+}
+
