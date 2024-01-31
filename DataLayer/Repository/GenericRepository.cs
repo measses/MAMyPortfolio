1ac2a75b4b291a699869c8787834a27a26e4d44d
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-01-31 20:25:39 +0300

    Repository changes

---
+++ DataLayer/Repository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repository;
+
+public class GenericRepository<T> : IGeneric<T> where T : class
+{
+    private readonly AppDbContext _context;
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public void Add(T t)
+    {
+        _context.Add(t);
+        _context.SaveChanges();
+    }
+
+    public void Delete(T t)
+    {
+        _context.Remove(t);
+        _context.SaveChanges();
+    }
+
+    public List<T> GetAll()
+    {
+        return _context.Set<T>().ToList();
+    }
+
+    public T GetById(int id)
+    {
+        return _context.Set<T>().Find(id);
+    }
+
+    public void Update(T t)
+    {
+        _context.Update(t);
+        _context.SaveChanges();
+    }
+}
