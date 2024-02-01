152648e29f4779a0589dfe1d4d8433900211d2e7
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-02-01 17:01:31 +0300

    add to theme and page partials

---
+++ MyPortfolio/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult HeroPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult JsPartial()
+        {
+            return PartialView();
+        }
+    }
+}
