f9aba3a87438eb8bdf10d82387df06a1279f0686
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-02-01 18:29:27 +0300

    Add to all Services

---
+++ BussinesLayer/Content/SkillManager.cs
@@ -0,0 +1,45 @@
+﻿using BussinesLayer.Services;
+using DataLayer.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Content;
+
+public class SkillManager : IGenericService<Skill>, ISkillService
+{
+    private readonly ISkill _skill;
+
+    public SkillManager(ISkill skill)
+    {
+        _skill = skill;
+    }
+
+    public void TAdd(Skill t)
+    {
+       _skill.Add(t);
+    }
+
+    public List<Skill> TGetAll()
+    {
+        return _skill.GetAll();
+    }
+
+    public Skill TGetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TRemove(Skill t)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TUpdate(Skill t)
+    {
+        throw new NotImplementedException();
+    }
+}
