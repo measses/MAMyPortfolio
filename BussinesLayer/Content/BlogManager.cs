7e84b6fce3b2efd1ee545c03d7005562b3c55dda
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-02-13 19:29:21 +0300

    update project

---
+++ BussinesLayer/Content/BlogManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinesLayer.Services;
+using DataLayer.Abstract;
+using Entities;
+
+namespace BussinesLayer.Content
+{
+    public class BlogManager : IGenericService<BlogDetail>,IBlogService
+    {
+        private readonly IBlog _blog;
+        public BlogManager(IBlog blog)
+        {
+            _blog = blog;
+        }
+
+        public void TAdd(BlogDetail t)
+        {
+           _blog.Add(t);
+        }
+
+        public List<BlogDetail> TGetAll()
+        {
+            return _blog.GetAll();
+        }
+
+        public BlogDetail TGetById(int id)
+        {
+            return (BlogDetail)_blog.GetById(id);
+        }
+
+        public void TRemove(BlogDetail t)
+        {
+            _blog.Delete(t);
+        }
+
+        public void TUpdate(BlogDetail t)
+        {
+            _blog.Update(t);
+        }
+    }
+}
---
+++ Entities/BlogDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class BlogDetail:BaseModel
+    {
+        public string Content { get; set; }
+        public string? Content2 { get; set; }
+        public string Image { get; set; }
+        public string? Image2 { get; set; }
+        public string? Image3 { get; set; }
+        public string Tag { get; set; }
+        public ICollection<BlogCategory> BlogCategory { get; set; }
+
+
+
+
+
+
+    }
+}
